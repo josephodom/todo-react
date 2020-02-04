9ca0a3c23f6cf891af5b64c4354301913e127338
josephodom <ejosephodom@gmail.com>
2020-02-04 14:53:55 -0500

    Started project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <main>
+            <header>
+                <div class="container">
+                    <h1>React Todo List</h1>
+                </div>
+            </header>
+            
+            <hr />
+            
+            <article>
+                <div class="container">
+                    <h2>To Do</h2>
+                </div>
+            </article>
+        </main>
+    );
+}
+
+export default App;
